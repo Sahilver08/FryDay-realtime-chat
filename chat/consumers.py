d197c8c3ed4b6ad50bd0ad419501a398c419111b
Sahil Verma <sahilver21430@gmail.com>
2026-02-14 22:34:23 +0530

    Add WebSocket routing and secure ChatConsumer with room membership validation

---
+++ chat/consumers.py
@@ -0,0 +1,52 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
+from .models import ChatRoom, RoomMember
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.user = self.scope['user']
+        self.room_uuid = self.scope['url_route']['kwargs']['room_uuid']
+        self.room_group_name = f'chat_{self.room_uuid}'
+
+        #reject anonymous user
+        if isinstance(self.user, AnonymousUser):
+            await self.close()
+            return
+
+        #checking if room exists 
+        try:
+            room = await ChatRoom.objects.aget(uuid=self.room_uuid)
+        except ChatRoom.DoesNotExist:
+            await self.close()
+            return
+
+        #checking if user is a member of the room
+        is_member = await RoomMember.objects.filter(
+            room = room,
+            user = self.user
+        ).aexists()
+
+        if not is_member:
+            await self.close()
+            return
+
+        # join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+        # leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        
--- config/asgi.py
+++ config/asgi.py
@@ -8,13 +8,24 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django.setup()
+
+import chat.routing
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
