7d6308fc3fd9adce44d3b50f47373815aba4ba62
Sahil Verma <sahilver21430@gmail.com>
2026-02-19 00:05:25 +0530

    Integrate Django Channels with ASGI, configure WebSocket routing, and implement secure ChatConsumer with room membership validation

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,22 +11,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_uuid = self.scope['url_route']['kwargs']['room_uuid']
         self.room_group_name = f'chat_{self.room_uuid}'
 
-        #reject anonymous user
+        print("WebSocket connect triggered")
+        print("User:", self.user)
+        print("Room UUID:", self.room_uuid)
+
+        # reject anonymous user
         if isinstance(self.user, AnonymousUser):
             await self.close()
             return
 
-        #checking if room exists 
+        # checking if room exists
         try:
             room = await ChatRoom.objects.aget(uuid=self.room_uuid)
         except ChatRoom.DoesNotExist:
             await self.close()
             return
 
-        #checking if user is a member of the room
+        # checking if user is a member of the room
         is_member = await RoomMember.objects.filter(
-            room = room,
-            user = self.user
+            room=room,
+            user=self.user
         ).aexists()
 
         if not is_member:
@@ -41,12 +45,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-
     async def disconnect(self, close_code):
         # leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-
-        
--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_uuid>[0-9a-fA-F]{32})/$', ChatConsumer.as_asgi()),
+    re_path(
+        r'^ws/chat/(?P<room_uuid>[0-9a-fA-F-]+)/$', ChatConsumer.as_asgi()),
 ]
