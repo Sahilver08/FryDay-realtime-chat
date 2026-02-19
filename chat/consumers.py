95c082e0610056f502258a6c59c18120800c5e19
Sahil Verma <sahilver21430@gmail.com>
2026-02-19 20:07:45 +0530

    Implement real-time WebSocket messaging with async DB persistence and group broadcasting

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
-from .models import ChatRoom, RoomMember
+from .models import ChatRoom, RoomMember, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -11,9 +11,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_uuid = self.scope['url_route']['kwargs']['room_uuid']
         self.room_group_name = f'chat_{self.room_uuid}'
 
-        print("WebSocket connect triggered")
-        print("User:", self.user)
-        print("Room UUID:", self.room_uuid)
+        # print("WebSocket connect triggered")
+        # print("User:", self.user)
+        # print("Room UUID:", self.room_uuid)
 
         # reject anonymous user
         if isinstance(self.user, AnonymousUser):
@@ -22,14 +22,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # checking if room exists
         try:
-            room = await ChatRoom.objects.aget(uuid=self.room_uuid)
+            self.room = await ChatRoom.objects.aget(uuid=self.room_uuid)
         except ChatRoom.DoesNotExist:
             await self.close()
             return
 
         # checking if user is a member of the room
         is_member = await RoomMember.objects.filter(
-            room=room,
+            room=self.room,
             user=self.user
         ).aexists()
 
@@ -51,3 +51,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        """Triggered when client sends message through websocket"""
+
+        data = json.loads(text_data)
+        content = data.get("content")
+
+        if not content:
+            return
+
+        # save message to database
+        message = await Message.objects.acreate(
+            room=self.room,
+            sender=self.user,
+            content=content,
+            message_type="text"
+
+        )
+
+        # broadcast to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message_id": message.id,
+                "content": message.content,
+                "sender": self.user.username,
+                "created_at": str(message.created_at)
+            }
+        )
+
+    async def chat_message(self, event):
+        """Triggered when a message is sent to the room group"""
+
+        await self.send(text_data=json.dumps({
+            "message_id": event["message_id"],
+            "content": event["content"],
+            "sender": event["sender"],
+            "created_at": event["created_at"]
+        }))
